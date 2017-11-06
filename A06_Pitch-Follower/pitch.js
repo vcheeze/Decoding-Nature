7972f1cbc2868002fb36f670128146a492453598
Peter Chen <manchesterutd7@gmail.com>
2017-11-06 14:59:47 +0400

    make boids more sensitive to pitch

--- A06_Pitch-Follower/pitch.js
+++ A06_Pitch-Follower/pitch.js
@@ -24,7 +24,6 @@ function setup() {
 function draw() {
 	background(51);
 
-
 	let micLevel = mic.getLevel();
 	print(micLevel);
 	micLevel = constrain(micLevel, 0.01, 0.55)
@@ -34,8 +33,8 @@ function draw() {
 	let spectrum = fft.analyze();
 	let centroid = fft.getCentroid(); // get the spectral centroid of the sound
 	// print(centroid);
-	centroid = constrain(centroid, 200, 4000);
-	centroid = map(centroid, 200, 4000, 0, width); // what values are best?
+	centroid = constrain(centroid, 1000, 4000);
+	centroid = map(centroid, 1000, 4000, 0, width); // what values are best?
 
 	flock.run(centroid, micLevel);
 }
