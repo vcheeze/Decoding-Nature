428559fc932e9c91963b1ad71ec1984585b97088
Peter Chen <manchesterutd7@gmail.com>
2017-12-03 23:01:28 +0400

    deleted unnecessary parts

--- A09_Maze3D/maze3D.js
+++ A09_Maze3D/maze3D.js
@@ -13,15 +13,8 @@ var pathAngleValues;
 var heroBaseY = 1.8;
 var bounceValue = 0.1;
 var gravity = 0.005;
-var leftLane = -1, rightLane = 1, middleLane = 0, currentLane;
 var clock;
-var jumping;
-var particleGeometry;
-var particleCount = 20;
-var explosionPower = 1.06;
-var particles;
-var scoreText, score;
-var hasCollided;
+
 
 init();
 
@@ -34,43 +27,32 @@ function init() {
 }
 
 function createScene() {
-	hasCollided=false;
-	score=0;
-	clock=new THREE.Clock();
+	clock = new THREE.Clock();
 	clock.start();
-	heroRollingSpeed=(rollingSpeed*worldRadius/heroRadius)/5;
+	heroRollingSpeed = ( rollingSpeed*worldRadius/heroRadius ) / 5;
 	sphericalHelper = new THREE.Spherical();
-	pathAngleValues=[1.52,1.57,1.62];
-    sceneWidth=window.innerWidth;
-    sceneHeight=window.innerHeight;
+	pathAngleValues = [ 1.52, 1.57, 1.62 ];
+    sceneWidth = window.innerWidth;
+    sceneHeight = window.innerHeight;
     scene = new THREE.Scene();//the 3d scene
     scene.fog = new THREE.FogExp2( 0xf0fff0, 0.14 );
-    camera = new THREE.PerspectiveCamera( 60, sceneWidth / sceneHeight, 0.1, 1000 );//perspective camera
-    renderer = new THREE.WebGLRenderer({alpha:true});//renderer with transparent backdrop
-    renderer.setClearColor(0xfffafa, 1);
-    renderer.shadowMap.enabled = true;//enable shadow
+    camera = new THREE.PerspectiveCamera( 60, sceneWidth/sceneHeight, 0.1, 1000 );//perspective camera
+    renderer = new THREE.WebGLRenderer( { alpha: true } );//renderer with transparent backdrop
+    renderer.setClearColor( 0xfffafa, 1 );
+    renderer.shadowMap.enabled = true; // enable shadow
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     renderer.setSize( sceneWidth, sceneHeight );
-    dom = document.getElementById('TutContainer');
-	dom.appendChild(renderer.domElement);
+    dom = document.getElementById( 'TutContainer' );
+	dom.appendChild( renderer.domElement );
 	addWorld();
 	addHero();
 	addLight();
 
 	camera.position.z = 6.5;
 	camera.position.y = 2.5;
-	window.addEventListener('resize', onWindowResize, false);//resize callback
+	window.addEventListener( 'resize', onWindowResize, false ); // resize callback
 
 	document.onkeydown = handleKeyDown;
-
-	scoreText = document.createElement('div');
-	scoreText.style.position = 'absolute';
-	scoreText.style.width = 100;
-	scoreText.style.height = 100;
-	scoreText.innerHTML = "0";
-	scoreText.style.top = 50 + 'px';
-	scoreText.style.left = 10 + 'px';
-	document.body.appendChild(scoreText);
 
   var infoText = document.createElement('div');
 	infoText.style.position = 'absolute';
@@ -83,106 +65,52 @@ function createScene() {
 	document.body.appendChild(infoText);
 }
 
-function handleKeyDown(keyEvent) {
-	if(jumping)return;
-	var validMove=true;
+function handleKeyDown( keyEvent ) {
 	if ( keyEvent.keyCode === 37) { // left
-		if(currentLane==middleLane){
-			currentLane=leftLane;
-		}else if(currentLane==rightLane){
-			currentLane=middleLane;
-		}else{
-			validMove=false;
-		}
+
 	} else if ( keyEvent.keyCode === 39) { // right
-		if(currentLane==middleLane){
-			currentLane=rightLane;
-		}else if(currentLane==leftLane){
-			currentLane=middleLane;
-		}else{
-			validMove=false;
+
+	} else {
+		if ( keyEvent.keyCode === 38 ) { // up, jump
+
 		}
-	}else{
-		if ( keyEvent.keyCode === 38){ // up, jump
-			bounceValue=0.1;
-			jumping=true;
-		}
-		validMove=false;
-	}
-	//heroSphere.position.x=currentLane;
-	if(validMove){
-		jumping=true;
-		bounceValue=0.06;
 	}
 }
 
 function addHero() {
 	var sphereGeometry = new THREE.DodecahedronGeometry( heroRadius, 1);
 	var sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xe5f2f2 ,shading:THREE.FlatShading} )
-	jumping=false;
 	heroSphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
 	heroSphere.receiveShadow = true;
-	heroSphere.castShadow=true;
+	heroSphere.castShadow = true;
 	scene.add( heroSphere );
-	heroSphere.position.y=heroBaseY;
-	heroSphere.position.z=4.8;
-	currentLane=middleLane;
-	heroSphere.position.x=currentLane;
+	heroSphere.position.y = heroBaseY;
+	heroSphere.position.z = 4.8;
 }
 
 function addWorld() {
-	var sides=40;
-	var tiers=40;
+	var sides = 40;
+	var tiers = 40;
 	var sphereGeometry = new THREE.SphereGeometry( worldRadius, sides,tiers);
-	var sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xfffafa ,shading:THREE.FlatShading} )
+	var sphereMaterial = new THREE.MeshStandardMaterial( { color: 0xfffafa, shading:THREE.FlatShading} )
 
-	var vertexIndex;
-	var vertexVector= new THREE.Vector3();
-	var nextVertexVector= new THREE.Vector3();
-	var firstVertexVector= new THREE.Vector3();
-	var offset= new THREE.Vector3();
-	var currentTier=1;
-	var lerpValue=0.5;
-	var heightValue;
-	var maxHeight=0.07;
-	for(var j=1;j<tiers-2;j++){
-		currentTier=j;
-		for(var i=0;i<sides;i++){
-			vertexIndex=(currentTier*sides)+1;
-			vertexVector=sphereGeometry.vertices[i+vertexIndex].clone();
-			if(j%2!==0){
-				if(i==0){
-					firstVertexVector=vertexVector.clone();
-				}
-				nextVertexVector=sphereGeometry.vertices[i+vertexIndex+1].clone();
-				if(i==sides-1){
-					nextVertexVector=firstVertexVector;
-				}
-				lerpValue=(Math.random()*(0.75-0.25))+0.25;
-				vertexVector.lerp(nextVertexVector,lerpValue);
-			}
-			heightValue=(Math.random()*maxHeight)-(maxHeight/2);
-			offset=vertexVector.clone().normalize().multiplyScalar(heightValue);
-			sphereGeometry.vertices[i+vertexIndex]=(vertexVector.add(offset));
-		}
-	}
 	rollingGroundSphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
 	rollingGroundSphere.receiveShadow = true;
-	rollingGroundSphere.castShadow=false;
-	rollingGroundSphere.rotation.z=-Math.PI/2;
+	rollingGroundSphere.castShadow = false;
+	rollingGroundSphere.rotation.z = -Math.PI/2;
 	scene.add( rollingGroundSphere );
-	rollingGroundSphere.position.y=-24;
-	rollingGroundSphere.position.z=2;
+	rollingGroundSphere.position.y = -24;
+	rollingGroundSphere.position.z = 2;
 }
 
 function addLight() {
-	var hemisphereLight = new THREE.HemisphereLight(0xfffafa,0x000000, .9)
-	scene.add(hemisphereLight);
-	sun = new THREE.DirectionalLight( 0xcdc1c5, 0.9);
-	sun.position.set( 12,6,-7 );
+	var hemisphereLight = new THREE.HemisphereLight( 0xfffafa, 0x000000, .9 )
+	scene.add( hemisphereLight );
+	sun = new THREE.DirectionalLight( 0xcdc1c5, 0.9 );
+	sun.position.set( 12, 6, -7 );
 	sun.castShadow = true;
-	scene.add(sun);
-	//Set up shadow properties for the sun light
+	scene.add( sun );
+	// set up shadow properties for the sun light
 	sun.shadow.mapSize.width = 256;
 	sun.shadow.mapSize.height = 256;
 	sun.shadow.camera.near = 0.5;
@@ -193,26 +121,25 @@ function update() {
   //animate
   rollingGroundSphere.rotation.x += rollingSpeed;
   heroSphere.rotation.x -= heroRollingSpeed;
-  if (heroSphere.position.y<=heroBaseY){
-  	jumping=false;
-  	bounceValue=(Math.random()*0.04)+0.005;
+  if (heroSphere.position.y <= heroBaseY){
+  	bounceValue = ( Math.random()*0.04 ) + 0.005;
   }
-  heroSphere.position.y+=bounceValue;
-  heroSphere.position.x=THREE.Math.lerp(heroSphere.position.x,currentLane, 2*clock.getDelta());//clock.getElapsedTime());
-  bounceValue-=gravity;
+  heroSphere.position.y += bounceValue;
+  heroSphere.position.x = THREE.Math.lerp( heroSphere.position.x, heroSphere.position.y, 2*clock.getDelta() );//clock.getElapsedTime());
+  bounceValue -= gravity;
   render();
-	requestAnimationFrame(update);//request next update
+	requestAnimationFrame( update ); // request next update
 }
 
 function render() {
-    renderer.render(scene, camera); // draw
+    renderer.render( scene, camera ); // draw
 }
 
 function onWindowResize() {
 	// resize & align
 	sceneHeight = window.innerHeight;
 	sceneWidth = window.innerWidth;
-	renderer.setSize(sceneWidth, sceneHeight);
-	camera.aspect = sceneWidth/sceneHeight;
+	renderer.setSize( sceneWidth, sceneHeight );
+	camera.aspect = sceneWidth / sceneHeight;
 	camera.updateProjectionMatrix();
 }
